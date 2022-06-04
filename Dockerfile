a298955fb40dd515de70117da1e7652744418b75
ghcdn <48314862+YulinChan@users.noreply.github.com>
2022-06-04 14:07:54 +0800

    rm conf

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM alpine:latest
 
-COPY entrypoint.sh /opt/entrypoint.sh
 COPY hls.js /var/www/hls.js
+COPY entrypoint.sh /opt/entrypoint.sh
 
 RUN apk add --no-cache nginx && \
     chmod a+x /opt/entrypoint.sh
-
-COPY default.conf /etc/nginx/http.d/default.conf
 
 ENTRYPOINT ["sh", "-c", "/opt/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,27 @@
+#!/bin/sh
+
 ## setup nginx
-sed -i "s/PORT/$PORT/g" /etc/nginx/http.d/default.conf
+cat << EOF > /etc/nginx/http.d/default.conf
+server {
+    listen ${PORT} default_server;
+    listen [::]:${PORT} default_server;
+    location / {
+        root   /var/www/hls.js;
+        index  index.html;
+    }
+    location /play {
+    if ($http_upgrade != "websocket") {
+        return 404;
+    }
+    proxy_pass http://127.0.0.1:9008;
+    proxy_redirect off;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection "upgrade";
+    proxy_set_header Host \$host;
+  }
+}
+EOF
 ## setup shadowsock-rust
 VERSION=$(wget --no-check-certificate -qO- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep 'tag_name' | cut -d\" -f4)
 SS_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/shadowsocks-${VERSION}.x86_64-unknown-linux-musl.tar.xz"
