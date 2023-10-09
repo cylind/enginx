b3225e28fa66c179bd18840cb042c1f4cf7d907d
ghcdn <cylind@proton.me>
2023-10-10 00:59:00 +0800

    add env

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,11 +23,11 @@ server {
 }
 EOF
 ## setup shadowsock-rust
-VERSION=$(wget -O- https://web-dl.pages.dev/https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep 'tag_name' | cut -d\" -f4)
+VERSION=$(wget -O- https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep 'tag_name' | cut -d\" -f4)
 SS_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/shadowsocks-${VERSION}.x86_64-unknown-linux-musl.tar.xz"
 wget ${SS_URL} && tar xf shadowsocks-*.tar.xz -C /usr/local/bin && rm shadowsocks-*.tar.xz && chmod a+x /usr/local/bin/ss*
 ## setup websocket-plugin
 wget 'https://dl.lamp.sh/files/v'2r'ay-plugin_linux_amd64' -qO /usr/local/bin/ws-plugin
 chmod +x /usr/local/bin/ws-plugin
 ## start service
-nginx && ssserver -s "127.0.0.1:9008" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "ws-plugin" --plugin-opts "server;path=/play"
+nginx && ssserver -s "127.0.0.1:9008" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "ws-plugin" --plugin-opts "server;path=${WSPATH}"
