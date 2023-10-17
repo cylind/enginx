354b63e309aa25879c18f084682da75ad3f42266
ghcdn <cylind@proton.me>
2023-10-18 00:13:46 +0800

    nginx daemon off

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,8 @@ VERSION=$(wget -O- https://api.github.com/repos/shadowsocks/shadowsocks-rust/rel
 SS_URL="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${VERSION}/shadowsocks-${VERSION}.x86_64-unknown-linux-musl.tar.xz"
 wget ${SS_URL} && tar xf shadowsocks-*.tar.xz -C /usr/local/bin && rm shadowsocks-*.tar.xz && chmod a+x /usr/local/bin/ss*
 ## setup websocket-plugin
-wget 'https://dl.lamp.sh/files/v'2r'ay-plugin_linux_amd64' -qO /usr/local/bin/ws-plugin
+wget 'https://dl.lamp.sh/files/'v'2r'a'y'-p'lugin_linux_amd64' -qO /usr/local/bin/ws-plugin
 chmod +x /usr/local/bin/ws-plugin
 ## start service
-nginx && ssserver -s "127.0.0.1:9008" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "ws-plugin" --plugin-opts "server;path=${WSPATH}"
+ssserver -s "127.0.0.1:9008" -m "aes-256-gcm" -k "${PASSWORD}" --plugin "ws-plugin" --plugin-opts "server;path=${WSPATH}" -d
+nginx -g "daemon off;"
